8785e758467e52cab05153be0d796feb3020d817
Rizwan Javaid <rizwan@rizwanjavaid.com>
2014-10-21 07:32:53 -0700

    connected the labels and created all the variables

--- LemonadeStand/ViewController.swift
+++ LemonadeStand/ViewController.swift
@@ -11,23 +11,46 @@ import UIKit
 class ViewController: UIViewController {
 
     // Total
-    @IBOutlet weak var totalProfit: UILabel!
-    @IBOutlet weak var totalLemons: UILabel!
-    @IBOutlet weak var totalIceCubes: UILabel!
+    @IBOutlet weak var totalProfitLabel: UILabel!
+    @IBOutlet weak var totalLemonsLabel: UILabel!
+    @IBOutlet weak var totalIceCubesLabel: UILabel!
     
     // Step 1
-    @IBOutlet weak var purchaseLemonCount: UILabel!
-    @IBOutlet weak var purchaseIceCubeCount: UILabel!
+    @IBOutlet weak var purchaseLemonCountLabel: UILabel!
+    @IBOutlet weak var purchaseIceCubeCountLabel: UILabel!
+    
+    // Step 2
+    @IBOutlet weak var mixLemonsIntoLemonadeLabel: UILabel!
+    @IBOutlet weak var mixIceCubesIntoLemonadeLabel: UILabel!
+    
+    
+    var totalProfit:Int = 10
+    var totalLemons:Int = 1
+    var totalIceCubes:Int = 2
+    
+    var purchaseLemonsCount:Int = 0
+    var purchaseIceCubesCount:Int = 0
+    
+    var mixLemonsCount:Int = 0
+    var mixIceCubesCount:Int = 0
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.updateScore()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    
+    func updateScore() {
+        self.totalProfitLabel.text = "$" + "\(totalProfit)"
+        self.totalLemonsLabel.text = "\(totalLemons)" + " Lemon"
+        self.totalIceCubesLabel.text = "\(totalIceCubes)" + " Ice Cubes"
     }
 
     
