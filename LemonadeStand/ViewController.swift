f0a03e40c612c23bbd4640b71167bd3a24f1d544
Rizwan Javaid <rizwan@rizwanjavaid.com>
2014-10-21 06:58:40 -0700

    Second Push

--- LemonadeStand/ViewController.swift
+++ LemonadeStand/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Total
+    @IBOutlet weak var totalProfit: UILabel!
+    @IBOutlet weak var totalLemons: UILabel!
+    @IBOutlet weak var totalIceCubes: UILabel!
+    
+    // Step 1
+    @IBOutlet weak var purchaseLemonCount: UILabel!
+    @IBOutlet weak var purchaseIceCubeCount: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +30,60 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    // Step 1 Actions
+    @IBAction func addLemonButtonPressed(sender: UIButton) {
+        
+    }
+    
+    @IBAction func removeLemonButtonPressed(sender: UIButton) {
+        
+    }
+    
+    @IBAction func addIceCubesButtonPressed(sender: UIButton) {
+    }
+    
+    @IBAction func removeIceCubeButtonPressed(sender: UIButton) {
+    }
+    
+    
+    
+    // Step 2 Actions
+    @IBAction func addLemonsToMixButtonPressed(sender: UIButton) {
+    }
+    
+    @IBAction func removeLemonsToMixButtonPressed(sender: UIButton) {
+    }
+    
+    @IBAction func addIceCubesToMixButtonPressed(sender: UIButton) {
+    }
+    
+    @IBAction func removeIceCubesToMixButtonPressed(sender: UIButton) {
+    }
+    
+    
+    
+    // Start Day Action
+    
+    @IBAction func startDayButtonPressed(sender: UIButton) {
+    }
+    
+    
+    
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
